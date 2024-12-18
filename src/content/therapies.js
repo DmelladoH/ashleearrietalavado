d23f06db1da588c89edcdfe91739b33b6d06fd3f
Daniel Mellado <mellado.m2@gmail.com>
2024-12-18 15:17:13 +0100

    feat: add swiper buttons

---
+++ src/content/therapies.js
@@ -0,0 +1,73 @@
+export const therapies = [
+  {
+    title: "para adultos",
+    onlinePrice: 60,
+    onlineTimeInMinutes: 50,
+    inPersonPrice: 70,
+    inPersonTimeInMinutes: 50,
+    description:
+      "En nuestro centro sabemos que la etapa adulta se caracteriza por estar llena de desafíos y por ello, buscamos ofrecer herramientas para la gestión emocional y el autoconocimiento. Se brinda un espacio seguro en el que no juzgamos e intentamos adaptarnos a cada situación e historia.",
+    details: [],
+  },
+  {
+    title: "infantil",
+    onlinePrice: 60,
+    onlineTimeInMinutes: 50,
+    inPersonPrice: 70,
+    inPersonTimeInMinutes: 50,
+    description:
+      "En nuestro centro consideramos vital una correcta atención de los menores, ya que se comprende la importancia de esta etapa, dándole importancia a un correcto desarrollo. Buscamos brindar un espacio seguro, de confianza y divertido para que puedan expresarse sin miedo, trabajando así la parte emocional. No solo nos centramos en el menor sino también los padres y madres.",
+    details: [],
+  },
+  {
+    title: "de adolescentes",
+    onlinePrice: 60,
+    onlineTimeInMinutes: 50,
+    inPersonPrice: 70,
+    inPersonTimeInMinutes: 50,
+    description:
+      "En nuestro centro sabemos que esta etapa se caracteriza por comportamientos desafiantes y difíciles de gestionar. En esta etapa los adolescentes pasan por diversos cambios que explican este tipo de comportamientos. Por ello, buscamos ofrecer al adolescente un espacio seguro y de confianza en el que ayudarle a entender estos cambios y brindar a los padres herramientas que les ayuden a entender, abordar y acompañar al adolescente.",
+    details: [],
+  },
+  {
+    title: "para personas mayores",
+    onlinePrice: 60,
+    onlineTimeInMinutes: 50,
+    inPersonPrice: 70,
+    inPersonTimeInMinutes: 50,
+    description:
+      "En nuestro centro se tiene en cuenta esta etapa vital, ya que se considera que es igual de relevante, debido a los cambios asociados a la edad. Por ello, buscamos ofrecer un espacio en el que brindar un espacio de escucha activa, comprensión y validación. Asimismo, se trabaja con estimulación cognitiva.",
+    details: [],
+  },
+  {
+    title: "de pareja",
+    onlinePrice: 80,
+    onlineTimeInMinutes: 60,
+    inPersonPrice: 90,
+    inPersonTimeInMinutes: 60,
+    description:
+      "En nuestro centro tenemos como objetivo ayudar a mejorar la calidad de las relaciones de pareja, ahondando en situaciones conflictivas que a veces es difícil abordar. Buscamos aumentar la satisfacción y ayudarles a superar desafíos que puedan surgir.",
+    details: [],
+  },
+  {
+    title: "de familia",
+    onlinePrice: 80,
+    onlineTimeInMinutes: 60,
+    inPersonPrice: 90,
+    inPersonTimeInMinutes: 60,
+    description:
+      "En nuestro centro entendemos que las relaciones familiares son importantes y que las situaciones que ocurran dentro del núcleo familiar a veces son difíciles de gestionar, pudiendo afectar a nuestro bienestar. Buscamos trabajar la comunicación entre todos los miembros creando un ambiente saludable.",
+    details: [],
+  },
+  {
+    title: "grupal",
+    onlinePrice: 30,
+    onlineTimeInMinutes: 60,
+    inPersonPrice: 40,
+    inPersonTimeInMinutes: 60,
+    description:
+      "En nuestro centro creemos importante que personas con una misma dificultad puedan ponerse en contacto. Buscamos trabajar diversas dificultades realizando sesiones dinámicas intentando poner en práctica sus habilidades, miedos, inseguridades, entre otros.",
+    details: [],
+    priceDisclaimer: "precio por persona",
+  },
+];
