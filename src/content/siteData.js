6018b1d29410b5caee1455b969ac4659433a333c
Daniel Mellado <mellado.m2@gmail.com>
2024-12-30 13:01:00 +0100

    feat: openGraph img

--- src/content/siteData.js
+++ src/content/siteData.js
@@ -3,5 +3,5 @@ export const siteData = {
   description:
     "Ashlee Arrieta Lavado, Psicóloga Sanitaria especializada en bienestar emocional, intervención escolar, TEA y Neuropsicología. Con formación en la Universidad de Deusto, brindo un espacio cercano y respetuoso para apoyar el crecimiento personal y afrontar desafíos sin temor al juicio.",
   canonical: "https://www.ashleearrietalavado.com",
-  image: "",
+  image: "/openGraph.png",
 };
