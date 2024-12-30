44652fab4f3f25f84bdaed95649271c71e013d92
Daniel Mellado <mellado.m2@gmail.com>
2024-12-30 12:48:25 +0100

    fix: address link

--- src/content/personalData.js
+++ src/content/personalData.js
@@ -3,7 +3,8 @@ export const personalData = {
   phone: "695 948 813",
   countryPhoneCode: "34",
   email: "centropsicologiaashlee@gmail.com",
-  address: "Gran Vía Don Diego López de Haro 81, Planta 3ª - Bilbao",
+  addressText: "Gran Vía Don Diego López de Haro 81, Planta 3ª - Bilbao",
+  addressLink: "https://maps.app.goo.gl/sJoqMDore6ZpUqwE9",
   psychologistNumber:
     "Colegiada Nº BI05868 en el Colegio de Psicología de Bizkaia",
   socialMedia: {
