55f01709f049dde17fe42c3a01e57c4a85953fac
Daniel Mellado <mellado.m2@gmail.com>
2024-12-30 12:35:39 +0100

    feat: add social media data

--- src/content/personalData.js
+++ src/content/personalData.js
@@ -1,13 +1,15 @@
 export const personalData = {
   fullname: "Ashlee Arrieta Lavado",
   phone: "695 948 813",
+  countryPhoneCode: "34",
   email: "centropsicologiaashlee@gmail.com",
   address: "Gran Vía Don Diego López de Haro 81, Planta 3ª - Bilbao",
   psychologistNumber:
     "Colegiada Nº BI05868 en el Colegio de Psicología de Bizkaia",
   socialMedia: {
-    linkedin: "https://www.linkedin.com/in/",
-    instagram: "https://www.instagram.com/",
-    doctoralia: "",
+    linkedin: "https://www.linkedin.com/in/ashlee-arrieta-lavado-655237224",
+    instagram: "https://www.instagram.com/ashleearrietalavado/",
+    doctoralia:
+      "https://www.doctoralia.es/ashlee-alanna-arrieta-lavado/psicologo/bilbao",
   },
 };
