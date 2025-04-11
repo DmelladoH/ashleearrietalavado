95061488b3edf7c72b58ea491fcd50ca2614c32e
Daniel Mellado <mellado.m2@gmail.com>
2025-04-11 20:36:04 +0200

    fix: prettier

--- src/content/experience.js
+++ src/content/experience.js
@@ -1,11 +1,11 @@
-import { getImage } from "astro:assets";
-import deusto from "@pictures/experience/deusto.png";
-import cop from "@pictures/experience/cop.svg";
-import medicosBilbao from "@pictures/experience/medicos-bilbao.png";
-import siac from "@pictures/experience/siac.png";
-import gac from "@pictures/experience/gac.png";
-import viu from "@pictures/experience/viu.png";
-import udima from "@pictures/experience/udima.svg";
+import { getImage } from 'astro:assets';
+import deusto from '@pictures/experience/deusto.png';
+import cop from '@pictures/experience/cop.svg';
+import medicosBilbao from '@pictures/experience/medicos-bilbao.png';
+import siac from '@pictures/experience/siac.png';
+import gac from '@pictures/experience/gac.png';
+import viu from '@pictures/experience/viu.png';
+import udima from '@pictures/experience/udima.svg';
 
 const width = 296;
 const height = 240;
@@ -13,63 +13,63 @@ const height = 240;
 export const experienceImgLinks = await Promise.all([
   getImage({
     src: deusto,
-    format: "webp",
+    format: 'webp',
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "Deusto",
+    name: 'Deusto',
   })),
   getImage({
     src: cop,
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "Colegio de Psicología de Bizkaia",
+    name: 'Colegio de Psicología de Bizkaia',
   })),
   getImage({
     src: medicosBilbao,
-    format: "webp",
+    format: 'webp',
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "Centro de Especialistas Médicos Bilbao, pediatría y adultos",
+    name: 'Centro de Especialistas Médicos Bilbao, pediatría y adultos',
   })),
   getImage({
     src: siac,
-    format: "webp",
+    format: 'webp',
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "SIAC",
+    name: 'SIAC',
   })),
   getImage({
     src: gac,
-    format: "webp",
+    format: 'webp',
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "GAC, Grupo ALBOR-COHS",
+    name: 'GAC, Grupo ALBOR-COHS',
   })),
   getImage({
     src: viu,
-    format: "webp",
+    format: 'webp',
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "VIU, Universidad Internacional de Valencia",
+    name: 'VIU, Universidad Internacional de Valencia',
   })),
   getImage({
     src: udima,
     width,
     height,
-  }).then((img) => ({
+  }).then(img => ({
     link: img.src,
-    name: "Udima, Universidad a distancia de Madrid",
+    name: 'Udima, Universidad a distancia de Madrid',
   })),
 ]);
