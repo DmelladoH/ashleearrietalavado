6baffede63cda68c9dab0037798c265f7afa8362
Daniel Mellado <mellado.m2@gmail.com>
2025-04-23 16:03:03 +0200

    feat: add reviews and service types

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface review {
+  reviewer: Person;
+  score: 1 | 2 | 3 | 4 | 5;
+  review: string;
+  source: ReviewSources;
+  link: string;
+}
+
+interface Person {
+  img: string;
+  fullName: string;
+}
+
+type ReviewSources = 'google' | 'doctoralia';
+
+export interface Service {
+  title: string;
+  onlinePrice: number;
+  onlineTimeInMinutes: number;
+  inPersonPrice: number;
+  inPersonTimeInMinutes: number;
+  priceDisclaimer?: string;
+  description: string;
+  details: string[];
+  summaryDetails: string[];
+}
