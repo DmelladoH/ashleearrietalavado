8ecf3561a08c36917f4fbbfde2b456945ee7961d
Daniel Mellado <mellado.m2@gmail.com>
2025-05-26 20:01:59 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://www.ashleearrietalavado.com/',
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: page => page !== 'https://www.ashleearrietalavado.com/admin/',
+    }),
+  ],
 });
